2e868e97e89baa7c910cbd3c49af1470aeefc8ee
Clément Levasseur <clevasseur@ad.gbs>
2019-02-15 15:09:50 -0500

    corrected init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-apt-get install --assume-yes nodejs
-apt-get install --assume-yes npm
-npm install -g serverless-artillery
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+sudo apt-get install --assume-yes nodejs
+sudo apt-get install --assume-yes npm
+npm install artillery
